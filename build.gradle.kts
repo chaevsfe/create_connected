b0394c50bc7a0131319e5555952ce1d9ee961c87
chaevsfe <pie@duck.com>
2026-09-23 01:33:23 -0400

    Hide disabled features in REI as in JEI

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
 
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     "clientCompileOnly"("mezz.jei:jei-26.2-fabric:30.24.0.165")
+    "clientCompileOnly"("maven.modrinth:rei:26.2.820+fabric")
     include(recipeViewer)
 }
 
